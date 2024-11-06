1c0a6dd16fa99b3beeae239a2330e07d566d71bf
ErSusant <susantanahak66@gmail.com>
2024-11-07 00:25:03 +0530

    Frontend

--- src/factory/Main.java
+++ src/factory/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
     EmployeeFactory emp=new EmployeeFactory();
-        Employee frontendDeveloper = emp.getEmployee("QA Tester");
+        Employee frontendDeveloper = emp.getEmployee("Frontend Developer");
         frontendDeveloper.roleDescription();
     }
 }
